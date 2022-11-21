896c1f081a5b3a81a1f839f398d4a3fc62567de8
hoangkimduc03 <duchkph28102@fpt.edu.vn>
2022-11-21 21:40:50 +0700

    h

--- pro1041_it17320_nhom5_finalproject/src/Model/PhieuBaoHanh.java
+++ pro1041_it17320_nhom5_finalproject/src/Model/PhieuBaoHanh.java
@@ -18,83 +18,12 @@ public class PhieuBaoHanh {
     private Date ngayBH;
     private Date thoiGianBH;
     private String tenKH;
-    private hdct hdct;
+    private String IDhdct;
 
     public PhieuBaoHanh() {
     }
 
-    public PhieuBaoHanh(String id, String ma, Date ngayBH, Date ngayMuaHang, String diaChi, Date thoiGianBH, String tenNguoiNhan) {
-        this.id = id;
-        this.ma = ma;
-        this.ngayBH = ngayBH;
-        this.ngayMuaHang = ngayMuaHang;
-        this.diaChi = diaChi;
-        this.thoiGianBH = thoiGianBH;
-        this.tenNguoiNhan = tenNguoiNhan;
-        
-    }
-
-    public PhieuBaoHanh(String id) {
-        this.id = id;
-    }
-    
-
    
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getMa() {
-        return ma;
-    }
-
-    public void setMa(String ma) {
-        this.ma = ma;
-    }
-
-    public Date getNgayBH() {
-        return ngayBH;
-    }
-
-    public void setNgayBH(Date ngayBH) {
-        this.ngayBH = ngayBH;
-    }
-
-    public Date getNgayMuaHang() {
-        return ngayMuaHang;
-    }
-
-    public void setNgayMuaHang(Date ngayMuaHang) {
-        this.ngayMuaHang = ngayMuaHang;
-    }
-
-    public String getDiaChi() {
-        return diaChi;
-    }
-
-    public void setDiaChi(String diaChi) {
-        this.diaChi = diaChi;
-    }
-
-    public Date getThoiGianBH() {
-        return thoiGianBH;
-    }
-
-    public void setThoiGianBH(Date thoiGianBH) {
-        this.thoiGianBH = thoiGianBH;
-    }
-
-    public String getTenNguoiNhan() {
-        return tenNguoiNhan;
-    }
-
-    public void setTenNguoiNhan(String tenNguoiNhan) {
-        this.tenNguoiNhan = tenNguoiNhan;
-    }
     
     
 }
